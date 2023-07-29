a77dca8cd6d5d45ae4ba36e0f3994b737390444a
2020630-cpe <90692461+2020630-cpe@users.noreply.github.com>
2023-07-29 22:54:45 +0800

    Add files via upload

---
+++ api_testing/connect.php
@@ -0,0 +1,36 @@
+<?php
+
+class db {
+
+	//declaration of host, database name, and database password
+	private $host = "localhost"; 
+	private $db_name = "Nelia";
+	private $db_user = "neliaaa"; 
+	private $db_pass = "Aguida_123"; 
+
+	public $conn;
+
+	//initiate connection to database		
+	public function get_connection() {
+		$this->conn = null;
+
+		try {
+			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->db_user, $this->db_pass);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->conn->exec("set names utf8");
+
+		} catch (PDOException $e) {
+			echo "Database connection failed: " . $e->getMessage();
+		}
+
+		return $this->conn;
+	}
+}
+
+// Create an instance of the db class
+$database = new db();
+
+// Attempt to establish the connection
+$connection = $database->get_connection();
+
+?>
